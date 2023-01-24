00e3acd12e0ab45fadfb62be05615ffc679f0553
Williamz21 <u201816224@upc.edu.pe>
2023-01-24 09:56:19 -0500

    minor changes

--- js/index.js
+++ js/index.js
@@ -4,6 +4,5 @@ const navbar = document.querySelector('.navbar');
 hamburguer.forEach(element => {
     element.addEventListener('click',()=>{
         navbar.classList.toggle('showbar')
-        console.log(9)
     })
 });
